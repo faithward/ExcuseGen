d3bc46e2e9c048e36ea39b969142e425326b8eee
faithward <faithward24@gmail.com>
2022-08-29 22:42:04 +0000

    almost working

--- src/app.js
+++ src/app.js
@@ -2,44 +2,45 @@
 import "bootstrap";
 import "./style.css";
 
-var subjects = [
-  "The dog",
-  "My grandma",
-  "A fascist",
-  "Joe Biden",
-  "Goku",
-  "A ghost from my past"
-];
-var action = ["ate", "peed on", "stole", "broke", "threw away"];
-var victim = [
-  "my homework",
-  "my clean clothes",
-  "my laptop",
-  "my car keys",
-  "my bike"
-];
-var time = [
-  "right before this.",
-  "on my way over.",
-  "yesterday.",
-  "this morning.",
-  "while I was distracted."
-];
-
-function generateExcuse() {
-  let str = "";
-  var subIndex = Math.floor(Math.random() * 6);
-  str += subjects[subIndex];
-  var actIndex = Math.floor(Math.random() * 5);
-  str += action[actIndex];
-  var victimIndex = Math.floor(Math.random() * 5);
-  str += victim[victimIndex];
-  var timeIndex = Math.floor(Math.random() * 5);
-  str += time[timeIndex];
-  return str;
-}
-
 window.onload = function() {
   //write your code here
-  console.log(generateExcuse());
+  var subjects = [
+    "The dog",
+    "My grandma",
+    "A fascist",
+    "Joe Biden",
+    "Goku",
+    "A ghost from my past"
+  ];
+  var action = [" ate", " peed on", " stole", " broke", " threw away"];
+  var victim = [
+    " my homework",
+    " my clean clothes",
+    " my laptop",
+    " my car keys",
+    " my bike"
+  ];
+  var time = [
+    " right before this.",
+    " on my way over.",
+    " yesterday.",
+    " this morning.",
+    " while I was distracted."
+  ];
+
+  function generateExcuse() {
+    let str = "";
+    var subIndex = Math.floor(Math.random() * 6);
+    str += subjects[subIndex];
+    var actIndex = Math.floor(Math.random() * 5);
+    str += action[actIndex];
+    var victimIndex = Math.floor(Math.random() * 5);
+    str += victim[victimIndex];
+    var timeIndex = Math.floor(Math.random() * 5);
+    str += time[timeIndex];
+    var h3 = document.querySelector("#myExcuse");
+    h3.innerText = str;
+  }
+
+  generateExcuse();
 };
