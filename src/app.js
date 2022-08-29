6db41666e9013dfe2254dcf435e241e2cd1fa205
faithward <faithward24@gmail.com>
2022-08-29 22:11:02 +0000

    edits

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+var subjects = [
+  "The dog",
+  "My grandma",
+  "A fascist",
+  "Joe Biden",
+  "Goku",
+  "A ghost from my past"
+];
+var action = ["ate", "peed on", "stole", "broke", "threw away"];
+var victim = [
+  "my homework",
+  "my clean clothes",
+  "my laptop",
+  "my car keys",
+  "my bike"
+];
+var time = [
+  "right before this.",
+  "on my way over.",
+  "yesterday.",
+  "this morning.",
+  "while I was distracted."
+];
+
+function generateExcuse() {
+  let str = "";
+  var subIndex = Math.floor(Math.random() * 6);
+  str += subjects[subIndex];
+  var actIndex = Math.floor(Math.random() * 5);
+  str += action[actIndex];
+  var victimIndex = Math.floor(Math.random() * 5);
+  str += victim[victimIndex];
+  var timeIndex = Math.floor(Math.random() * 5);
+  str += time[timeIndex];
+  return str;
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log(generateExcuse());
 };
